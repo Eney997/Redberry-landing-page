620dd612731b0ff13521512f0423f700be8abb94
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2023-12-19 17:21:16 +0400

    Popup:added

---
+++ app.js
@@ -0,0 +1,41 @@
+const openModals = document.querySelectorAll('[data-modal-target]');
+const blur = document.getElementById('blur');
+
+openModals.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalTarget = button.getAttribute('data-modal-target');
+        const popup = document.querySelector(modalTarget);
+        openModal(popup);
+    });
+});
+
+function openModal(modal) {
+    if (modal == null) return;
+
+    if (!modal.classList.contains('active')) {
+        modal.classList.add('active');
+        blur.classList.add('active');
+    } else {
+        modal.classList.remove('active');
+        blur.classList.remove('active');
+    }
+}
+
+
+blur.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.popInfo.active');
+    modals.forEach(modal => {
+        closeModal(modal);
+    });
+});
+
+function closeModal(modal) {
+    if (modal == null) return;
+
+    modal.classList.remove('active');
+    blur.classList.remove('active');
+}
+
+
+
+
