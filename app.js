a5d5c146605f5dcee14fe4ceeb758f5a8dfdf7ca
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2023-12-20 15:00:16 +0400

    login:added

--- app.js
+++ app.js
@@ -38,4 +38,60 @@ function closeModal(modal) {
 
 
 
+    const popInfo = document.getElementById('popInfo');
+    const blur1 = document.getElementById('blur');
+    const emailInput = document.querySelector('.popinput');
+    const errorContainer = document.querySelector('.errorsinPosta');
+    const successPopup = document.querySelector('.succsessLogin');
+    const inpopbut = document.querySelector('.inpopbut');
 
+    const apiUrl = 'https://api.blog.redberryinternship.ge/api/login';
+
+    inpopbut.addEventListener('click', function () {
+        const email = emailInput.value;
+
+        // reset eroroebi
+        errorContainer.style.display = 'none';
+        successPopup.style.display = 'none';
+
+        // email checki
+        if (!isValidEmail(email)) {
+            errorContainer.style.display = 'flex';
+            return;
+        }
+
+        // fetch req
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'accept': 'application/json',        
+            },
+            body: JSON.stringify({email}),
+        })
+        .then(response => response.json())
+        .then(data => handleApiResponse(data))
+        .catch(error => console.error('Error:', error));
+    });
+
+    function isValidEmail(email) {
+        // email validacia
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
+    function handleApiResponse(data) {
+        if (data.message === 'User logged in successfully') {
+            // succes popup visible
+            successPopup.style.display = 'grid';
+            popInfo.style.display = 'none';
+        } else {
+            // gavhendlot erori
+            errorContainer.style.display = 'flex';
+            document.querySelector('.errorPosta').innerText = data.message;
+        }
+    }
+
+
+
+    
