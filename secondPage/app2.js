a4818e76da8a005ad84844337096e427ba78b07a
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2023-12-22 15:03:43 +0400

    secondPage:Login

--- secondPage/app2.js
+++ secondPage/app2.js
@@ -37,5 +37,98 @@ function closeModal(modal) {
 }
 
 
+const popInfo = document.getElementById('popInfo');
+const blur1 = document.getElementById('blur');
+const emailInput = document.querySelector('.popinput');
+const errorContainer = document.querySelector('.errorsinPosta');
+const successPopup = document.querySelector('.succsessLogin');
+const inpopbut = document.querySelector('.inpopbut');
+const disBlock = document.querySelector('.disBlock');
+const Slb = document.querySelector('.SLB');
+const Hbut = document.querySelector('.Hbut'); 
+const ABBbutton = document.querySelector('.addBlog'); 
+
+const apiUrl = 'https://api.blog.redberryinternship.ge/api/login';
+
+// localStorage Clear aq chavakomentarot es imitoto sheyvanis mere davasuftaot
+// localStorage.clear();
+
+// Function to close the popup, remove the blur, and button visibility
+function closePopup() {
+    popInfo.style.display = 'none';
+    blur1.style.display = 'none';
+
+    // button visibility
+    Hbut.style.display = 'none';
+    ABBbutton.style.display = 'block';
+
+    // login state in localStorage
+    localStorage.setItem('isLoggedIn', 'true');
+}
+
+// Event listener for "SLB" button
+Slb.addEventListener('click', closePopup);
+
+// Check localStorage on page load
+if (localStorage.getItem('isLoggedIn') === 'true') {
+    // show login button and hide the addBlog button //hide and sick
+    Hbut.style.display = 'none';
+    ABBbutton.style.display = 'block';
+}
+
+inpopbut.addEventListener('click', function () {
+    const email = emailInput.value;
+    
+    // reset eroroebi
+    errorContainer.style.display = 'none';
+    successPopup.style.display = 'none';
+    
+    // email checki
+    if (!isValidEmail(email)) {
+        errorContainer.style.display = 'flex';
+        return;
+    }
+    
+    // fetch req
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'accept': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+    })
+    .then(response => {
+        if (response.status === 204) {
+            // success popup visible
+            successPopup.style.display = 'grid';
+            disBlock.style.display = 'none';
+            
+            // butonis visibiliti
+            Hbut.style.display = 'block';
+            ABBbutton.style.display = 'none';
+            
+            // login in localStorage
+            localStorage.setItem('isLoggedIn', 'true');
+        } else {
+            return response.json();
+        }
+    })
+    .then(data => {
+        if (data && data.message) {
+            // sxva erorebis gahendvla
+            errorContainer.style.display = 'flex';
+            document.querySelector('.errorPosta').innerText = data.message;
+        }
+    })
+    .catch(error => console.error('Error:', error));
+});
+
+function isValidEmail(email) {
+    // email validacia
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
 
 
